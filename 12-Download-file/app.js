33e151e8fbab6b09c64c5a62bad8bd7c48fa6743
Daniel Dormin <danielpinhalfilho@yahoo.com.br>
2021-06-15 03:25:03 -0300

    download de arquivo completo

---
+++ 12-Download-file/app.js
@@ -0,0 +1,21 @@
+const download = (filename, text) =>{
+    let item = document.createElement('a')
+    item.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+    item.setAttribute('download', filename)
+
+    item.style.display = none
+    document.body.appendChild(item)
+    console.log(item)
+    item.click()
+    document.body.removeChild(item)
+
+    // gera esse codigo no html
+    //< a href = "data:text/plain;charset=utf-8, teste" download = 'filename' > teste</a >
+
+}
+
+document.getElementById('btn').addEventListener("click", function(){
+    var txt = document.getElementById("text").value
+    var filename = "download.txt"
+    download(filename, txt)
+}, false)
